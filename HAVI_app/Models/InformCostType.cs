3a006414b055d93974aa5b7bc171848ff3097433
cfog19 <55488203+cfog19@users.noreply.github.com>
2020-11-17 10:15:34 +0100

    Added robot fields and dropmenus

---
+++ HAVI_app/Models/InformCostType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HAVI_app.Models
+{
+    public class InformCostType
+    {
+        public int ID { get; set; }
+        public string CostType { get; set; }
+    }
+}
