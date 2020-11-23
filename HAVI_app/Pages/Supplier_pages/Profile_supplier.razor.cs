13f400d8eed593d48ef7204071c9a117f26ba6c6
Cecilie Fog <55488203+cfog19@users.noreply.github.com>
2020-11-23 10:18:12 +0100

    Ved ikke hvad jeg har lavet

---
+++ HAVI_app/Pages/Supplier_pages/Profile_supplier.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HAVI_app.Models;
+using Microsoft.JSInterop;
+using static System.Net.WebRequestMethods;
+
+namespace HAVI_app.Pages.Supplier_pages
+{
+    public partial class Profile_supplier
+    {
+        public static readonly HttpClient client = new HttpClient();
+        Supplier supplier = new Supplier();
+        protected async override Task OnParametersSetAsync()
+        {
+            try
+            {
+                Uri url = new Uri("api/supplier");
+                HttpResponseMessage response = await client.GetAsync(url);
+                Console.WriteLine(response);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
