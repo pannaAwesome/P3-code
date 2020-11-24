a06f7507a1f8a7130565a02dd8451b163cfec105
ESMA3000 <58027922+ESMA3000@users.noreply.github.com>
2020-11-24 10:01:44 +0100

    validation methods are fixed

--- HAVI_app/Classes/Validation.cs
+++ HAVI_app/Classes/Validation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace HAVI_app.Classes
 {
-    public class Validation
+    public class Validation : ComponentBase
     {
         public bool LettersOnly(string input)
         {
