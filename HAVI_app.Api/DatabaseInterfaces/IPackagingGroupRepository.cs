975bf0d2fe30f896f55165fd8bd6f0feef9ee6cc
Cecilie Fog <55488203+cfog19@users.noreply.github.com>
2020-11-24 09:18:14 +0100

    Added somehing

---
+++ HAVI_app.Api/DatabaseInterfaces/IPackagingGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using HAVI_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HAVI_app.Api.DatabaseInterfaces
+{
+    interface IPackagingGroupRepository
+    {
+        Task<IEnumerable<PackagingGroup>> GetPackagingGroups();
+    }
+}
