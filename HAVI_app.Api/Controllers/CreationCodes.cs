fa52856aad2bec86c674b6bce7337953fe864614
Cecilie Fog <55488203+cfog19@users.noreply.github.com>
2020-11-25 10:42:25 +0100

    Added CreationCodesController

---
+++ HAVI_app.Api/Controllers/CreationCodes.cs
@@ -0,0 +1,126 @@
+﻿using HAVI_app.Api.DatabaseInterfaces;
+using HAVI_app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HAVI_app.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreationCodes : ControllerBase
+    {
+        private readonly ICreationCodeRepository _creationCodeRepository;
+        public CreationCodesController(ICreationCodeRepository creationCodeRepository)
+        {
+            _creationCodeRepository = creationCodeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCreationCodes()
+        {
+            try
+            {
+                var result = await _creationCodeRepository.GetCreationCodes();
+                if(result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database.");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CreationCode>> GetCreationCode(int id)
+        {
+            try
+            {
+                var result = await _creationCodeRepository.GetCreationCode(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CreationCode>> CreateCreationCode(CreationCode creationCode)
+        {
+            try
+            {
+                if (creationCode == null)
+                {
+                    return BadRequest();
+                }
+
+                var createdCreationCode = await _creationCodeRepository.AddCreationCode(creationCode);
+
+                return CreatedAtAction(nameof(GetCreationCode), new { id = createdCreationCode.Id }, createdCreationCode);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CreationCode>> UpdateCreationCode(int id, CreationCode creationCode)
+        {
+            try
+            {
+                if (id != creationCode.Id)
+                {
+                    return BadRequest();
+                }
+
+                var creationCodeToUpdate = await _creationCodeRepository.GetCreationCode(id);
+
+                if (creationCodeToUpdate == null)
+                {
+                    return NotFound($"CreationCode with id = {id} not found");
+                }
+
+                return await _creationCodeRepository.UpdateCreationCode(creationCode);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CreationCode>> DeleteCreationCode(int id)
+        {
+            try
+            {
+                var creationCodeToDelete = await _creationCodeRepository.GetCreationCode(id);
+
+                if (creationCodeToDelete == null)
+                {
+                    return NotFound($"CreationCode with id = {id} not found");
+                }
+
+                return await _creationCodeRepository.DeleteCreationCodeAsync(id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database.");
+            }
+        }
+    }
+}
