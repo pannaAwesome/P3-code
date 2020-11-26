217af63b564175cb3b8b14e456cb7213bbb7e049
Cecilie Fog <55488203+cfog19@users.noreply.github.com>
2020-11-26 12:02:33 +0100

    Added ArticleBundleServide

---
+++ HAVI_app/Services/Classes/ArticleBundleService.cs
@@ -0,0 +1,23 @@
+﻿using HAVI_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace HAVI_app.Services.Classes
+{
+    public class ArticleBundleService
+    {
+        private readonly HttpClient httpClient;
+        public ArticleBundleService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IEnumerable<ArticleBundle>> GetSuppliers()
+        {
+            return await httpClient.GetFromJsonAsync<ArticleBundle[]>("/api/articleBundles");
+        }
+    }
+}
