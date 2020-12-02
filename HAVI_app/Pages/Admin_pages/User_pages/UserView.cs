3f07d6e885769cbf3e15a0b4d9859564898df3d3
pannaAwesome <pannaogpiko@gmail.com>
2020-12-02 11:42:52 +0100

    Fixed bug in supplierrepository

--- HAVI_app/Pages/Admin_pages/User_pages/UserView.cs
+++ HAVI_app/Pages/Admin_pages/User_pages/UserView.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using HAVI_app.Models;
+using HAVI_app.Services.Classes;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,14 @@ namespace HAVI_app.Pages.Admin_pages.User_pages
 {
     public class UserView : ComponentBase
     {
-    [Parameter]
+        [Parameter]
         public static int Id { get; set; }
+
+        [Inject]
+        public PurchaserService PurchaserService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
 
         public string profile = $"/profile_admin/{Id}";
         public string country = $"/country_view/{Id}";
@@ -18,5 +26,21 @@ namespace HAVI_app.Pages.Admin_pages.User_pages
         public string overview = $"/overview_admin/{Id}";
 
         public string Email { get; set; }
-}
+
+        public async Task CreatePurchaser()
+        {
+            Purchaser purchaser = new Purchaser()
+            {
+                CountryId = Id,
+                Profile = new Profile()
+            };
+
+            purchaser.Profile.Username = Email;
+            purchaser.Profile.Password = "1234";
+
+            await PurchaserService.CreatePurchaser(purchaser);
+
+            NavigationManager.NavigateTo($"/user_view/{Id}", true);
+        }
+    }
 }
