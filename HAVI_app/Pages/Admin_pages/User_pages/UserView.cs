8894f2ace5fa8f138f9b9f35705334dc005d7a42
pannaAwesome <pannaogpiko@gmail.com>
2020-12-02 09:03:49 +0100

    Added admin stuff

--- HAVI_app/Pages/Admin_pages/User_pages/UserView.cs
+++ HAVI_app/Pages/Admin_pages/User_pages/UserView.cs
@@ -8,6 +8,15 @@ namespace HAVI_app.Pages.Admin_pages.User_pages
 {
     public class UserView : ComponentBase
     {
+    [Parameter]
+        public static int Id { get; set; }
 
-    }
+        public string profile = $"/profile_admin/{Id}";
+        public string country = $"/country_view/{Id}";
+        public string user = $"/user_view/{Id}";
+        public string article = $"/article_view/{Id}";
+        public string overview = $"/overview_admin/{Id}";
+
+        public string Email { get; set; }
+}
 }
