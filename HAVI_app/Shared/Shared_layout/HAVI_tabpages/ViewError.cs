4853f5a96b24994f5105b6c9074784448eda54bf
pannaAwesome <pannaogpiko@gmail.com>
2020-12-03 14:33:04 +0100

    Fixed view error

--- HAVI_app/Shared/Shared_layout/HAVI_tabpages/ViewError.cs
+++ HAVI_app/Shared/Shared_layout/HAVI_tabpages/ViewError.cs
@@ -1,4 +1,6 @@
-﻿using HAVI_app.Models;
+﻿using HAVI_app.Classes;
+using HAVI_app.Models;
+using HAVI_app.Services.Classes;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -12,18 +14,25 @@ namespace HAVI_app.Shared.Shared_layout.HAVI_tabpages
         [Parameter]
         public int ArticleId { get; set; }
 
-
+        [Inject]
+        public ArticleService ArticleService { get; set; }
 
         public Article Article { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-           
+            Article = await ArticleService.GetArticle(ArticleId);
         }
 
         public async void ErrorFixed()
         {
+            Article.ErrorReported = 0;
+            Article.ErrorOwner = "";
+            Article.ErrorMessage = "";
+            Article.ErrorField = "";
+            Article.ArticleState = (int)ArticleState.RobotReady;
 
+            await ArticleService.UpdateArticle(ArticleId, Article);
         }
     }
 }
